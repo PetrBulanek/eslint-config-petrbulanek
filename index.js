4cc28762c175434e5e99b992169490263000ccd2
Petr Bulánek <bulanek.petr@gmail.com>
2022-01-09 22:24:15 +0100

    feat: setup basic ESLint rules

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = {};
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	plugins: ['prettier'],
+	settings: {
+		react: {
+			version: 'latest',
+		},
+	},
+	rules: {
+		'prettier/prettier': ['error', require('./.prettierrc.js')],
+	},
+};
